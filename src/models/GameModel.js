9e4ec42a50e621f36eb0d06442a6b5b74711e411
HashtAggie <a.el.arras@gmail.com>
2016-05-02 14:06:22 +0200

    update

--- src/models/GameModel.js
+++ src/models/GameModel.js
@@ -1,6 +1,15 @@
 import BaseModel from './BaseModel';
 
 class GameModel extends BaseModel {
+  defaults() {
+    return {
+      playerOne: null,
+      playerTwo: null,
+      playerOneMove: null,
+      playerTwoMove: null,
+      winner: null
+    };
+  }
   constructor() {
     super('game');
   }
